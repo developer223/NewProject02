71cbb92c9d918aea8f107a2a6b4d90ec1b30f4e5
developer223 <1758875192@qq.com>
2020-04-24 15:50:35 +0800

    4.24

--- app/src/main/java/sn170507180223/classroom/android02/sdwu/edu/cn/newproject02/ch13Activity_1.java
+++ app/src/main/java/sn170507180223/classroom/android02/sdwu/edu/cn/newproject02/ch13Activity_1.java
@@ -1,19 +1,34 @@
 package sn170507180223.classroom.android02.sdwu.edu.cn.newproject02;
 
+import android.content.SharedPreferences;
+import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class ch13Activity_1 extends AppCompatActivity {
-
+    private EditText ip;
+    private EditText port;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ch13_1);
+
+        ip=(EditText)findViewById(R.id.ch13_1_ip);
+        port=(EditText)findViewById(R.id.ch13_1_port);
+
+        SharedPreferences sharedPreferencesCompat=getSharedPreferences("prefs",MODE_PRIVATE);
+        ip.setText(sharedPreferencesCompat.getString("ip",""));
+        port.setText(sharedPreferencesCompat.getString("port","" ));
+
+
+
     }
     private void write(View v){
         EditText editText=(EditText)findViewById(R.id.ch13_1_et);
@@ -27,5 +42,26 @@ public class ch13Activity_1 extends AppCompatActivity {
         }catch (Exception e){
             Log.e(ch13Activity_1.class.toString(),e.toString());
         }
+    }
+    public void read(View view){
+        try{
+            FileInputStream fileInputStream=openFileInput("android01.txt");
+            int size=fileInputStream.available();
+            byte[] bytes=new byte[size];
+            fileInputStream.read(bytes);
+            String content=new String(bytes);
+
+            fileInputStream.close();
+            Toast.makeText(this,content,Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Log.e(ch13Activity_1.class.toString(),e.toString());
+        }
+    }
+    public void saveSharePref(View view){
+        SharedPreferences sharedPreferencesCompat=getSharedPreferences("prefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferencesCompat.edit();
+        editor.putString("ip",ip.getText().toString());
+        editor.putString("port",port.getText().toString());
+        editor.commit();
     }
 }
